265487e782d26e7db00d66247781406fe78d17d7
Peter Popov <dzeensibir@yandex.ru>
2023-02-14 22:49:58 +0500

    style: Добавить пустые строки2

--- src/main/java/ru/practicum/shareit/exception/NotFoundException.java
+++ src/main/java/ru/practicum/shareit/exception/NotFoundException.java
@@ -5,7 +5,9 @@ public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
     }
+
     public NotFoundException(String clsName, Long id) {
         super(String.format("%s с ID = %d не существует", clsName, id));
     }
+
 }
